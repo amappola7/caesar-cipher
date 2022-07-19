39c8d87295a8210b4353e324707b41f22acb13e4
Amappola <anam.porrasp@gmail.com>
2022-07-19 11:07:40 -0500

    Versión estable. Funcionalidad: Cifrado únicamente

--- src/cipher.js
+++ src/cipher.js
@@ -1,14 +1,10 @@
 // Export functions to index.js
 const cipher = {
   encode: encryptFunction,
-
-  decode: function decryptFunction(key, text) {
-  }
+  decode: decryptFunction
 };
 
-export default cipher;
-// export default { encryptFunction, decryptFunction }
-
+// Encrypt Function
 function encryptFunction(key, text) {
   let cipherText = [];
 
@@ -39,56 +35,22 @@ function encryptFunction(key, text) {
   return cipherText.join("");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Encrypt Function
-// function encryptFunction(key, text) {
-//   let cipherText = [];
-
-//   for (let i = 0; i < text.length; i++) {
-//     let letterCode = text.charCodeAt(i);
-//     let cipherLetter;
-//     let cipherCode;
-
-//     if (letterCode >= 65 && letterCode <= 90) {
-
-//       cipherCode = (letterCode - 65 + key) % 26 + 65;
-//       cipherLetter = String.fromCharCode(cipherCode);
-//       cipherText.push(cipherLetter);
-
-//     } else if (letterCode >= 97 && letterCode <= 122) {
-
-//       cipherCode = (letterCode - 97 + key) % 26 + 97;
-//       cipherLetter = String.fromCharCode(cipherCode);
-//       cipherText.push(cipherLetter);
-
-//     } else {
-
-//       cipherText.push(text[i]);
-
-//     }
-
-//   };
-//   console.log(cipherText.join(""));
-//   return cipherText.join("");
-// }
-
-// encryptFunction(1, "Hola");
-
 // Decrypt Function
-// function decryptFunction(key, text) {
-// };
+function decryptFunction(key, text) {
+}
+
+// Export functions to index.js
+export default cipher;
+
+
+
+
+
+
+
+
+
+
+
+
+
